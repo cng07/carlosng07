68c782c71747509f00678a71cc2e01e4f289665f
carlosNg <carlosng07@gmail.com>
2026-02-02 20:17:57 +0800

    Fix: resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -34,8 +34,8 @@ const Resume = () => {
                         style={{ borderRadius: '8px' }}
                     >
                         <iframe
-                            src={`https://drive.google.com/embeddedplayer?fileId=${encodeURIComponent('1GnW2Cdlc5pLg-yTtz0YENjrwEJnHAqbn')}&embedded=true`}
-                            // src={`https://drive.google.com/file/d/1GnW2Cdlc5pLg-yTtz0YENjrwEJnHAqbn/preview`}
+                            // src={`https://drive.google.com/embeddedplayer?fileId=${encodeURIComponent('1GnW2Cdlc5pLg-yTtz0YENjrwEJnHAqbn')}&embedded=true`}
+                            src={`https://drive.google.com/file/d/1GnW2Cdlc5pLg-yTtz0YENjrwEJnHAqbn/preview`}
                             title="Carlos Ng Resume"
                             width="100%"
                             height="100%"
